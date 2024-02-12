2b99a921c52348a8e773151c25bc739796cc0c1a
yodahe <94496398+yodaheYT@users.noreply.github.com>
2024-02-11 17:07:39 -0700

    Version 0.1

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Homelab Television (HLTV)
+## A simple tool made in Java using VLCJ to randomly play files in a directory
+
+| Release | Link                                   | Status   |
+|---------|----------------------------------------|----------|
+| Stable  | [GitHub Releases](https://github.com/) | Healthy  |
--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,9 +1,14 @@
 package main;
 
 public class Main {
+    public static String DefaultPath = "";
     public static DirectoryPlaylistMaker playlistMaker = new DirectoryPlaylistMaker();
     public static void main(String[] args) {
-        playlistMaker.loadDir("\\\\clever.local\\Server Files\\08. Vid");
+        if (args[0] != null) {
+            playlistMaker.loadDir(args[0]);
+        } else {
+            playlistMaker.loadDir(DefaultPath);
+        }
         playlistMaker.randomize();
 
         Display display = new Display(playlistMaker);
